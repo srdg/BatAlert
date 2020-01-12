7f710d9e7e9b11fd30afac9112e8e6693cbad829
srdg <srd1908@cse.jgec.ac.in>
2020-01-12 09:48:45 +0530

    alert notifier for ubuntu 18.04

---
+++ sh/ubuntu18.batalert.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+load_notifier()
+{
+
+	 if [[ $( cat /sys/class/power_supply/BAT1/uevent | grep -i status | cut -d'=' -f2 ) == "Discharging" ]] && (( $( cat /sys/class/power_supply/BAT1/uevent | grep -i capacity | head -n 1 | cut -d'=' -f2  ) <= 60 )); then notify-send -i face-worried "Battery low" "Please plug in the charger"; return 1; fi
+
+	 if [[ $( cat /sys/class/power_supply/BAT1/uevent | grep -i status | cut -d'=' -f2 ) == "Full" ]] && (( $( cat /sys/class/power_supply/BAT1/uevent | grep -i capacity | head -n 1 | cut -d'=' -f2  ) == 100 )); then notify-send -i face-happy "Battery full" "You can  unplug the charger"; return 1; fi
+
+	
+}
+
+while true
+do
+	load_notifier;
+	sleep 10s;
+	
+done
