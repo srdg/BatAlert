47d1d2c0852985e8f7bdf91a6ab713c76b1ee7ec
srdg <20687315+srdg@users.noreply.github.com>
2020-01-12 09:59:52 +0530

    reconfigure alert percentage

--- sh/ubuntu18.batalert.sh
+++ sh/ubuntu18.batalert.sh
@@ -2,7 +2,7 @@
 load_notifier()
 {
 
-	 if [[ $( cat /sys/class/power_supply/BAT1/uevent | grep -i status | cut -d'=' -f2 ) == "Discharging" ]] && (( $( cat /sys/class/power_supply/BAT1/uevent | grep -i capacity | head -n 1 | cut -d'=' -f2  ) <= 60 )); then notify-send -i face-worried "Battery low" "Please plug in the charger"; return 1; fi
+	 if [[ $( cat /sys/class/power_supply/BAT1/uevent | grep -i status | cut -d'=' -f2 ) == "Discharging" ]] && (( $( cat /sys/class/power_supply/BAT1/uevent | grep -i capacity | head -n 1 | cut -d'=' -f2  ) <= 40 )); then notify-send -i face-worried "Battery low" "Please plug in the charger"; return 1; fi
 
 	 if [[ $( cat /sys/class/power_supply/BAT1/uevent | grep -i status | cut -d'=' -f2 ) == "Full" ]] && (( $( cat /sys/class/power_supply/BAT1/uevent | grep -i capacity | head -n 1 | cut -d'=' -f2  ) == 100 )); then notify-send -i face-happy "Battery full" "You can  unplug the charger"; return 1; fi
 
